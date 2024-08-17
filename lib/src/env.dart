28129915e2a6fcf3bf9299a851a3e08eb8c7060a
mcmah309 <dillonmcmahon79@gmail.com>
2024-08-17 19:27:45 +0000

    feat: Add path_provider functionality

--- lib/src/env.dart
+++ lib/src/env.dart
@@ -1,9 +1,14 @@
 import 'dart:io';
 
+import 'package:path_provider/path_provider.dart';
 import 'package:rust_core/rust_core.dart';
 
+/// Inspection and manipulation of the process’s environment.
+///
+/// This module contains functions to inspect various aspects such as environment variables, the current directory, and various other important directories.
 class Env {
   /// Returns the current working directory as a Path.
+  @pragma('vm:prefer-inline')
   static String currentDirectory() => Directory.current.path;
 
   /// The path of the executable used to run the script in this isolate.
@@ -15,6 +20,7 @@ class Env {
   /// was found by searching the system path.
   ///
   /// Use [resolvedExecutable] to get an absolute path to the executable.
+  @pragma('vm:prefer-inline')
   static String currentExe() => Platform.executable;
 
   /// Joins a collection of Paths appropriately for the PATH environment variable.
@@ -30,12 +36,51 @@ class Env {
   }
 
   /// Returns an iterator of (variable, value) pairs of strings, for all the environment variables of the current process.
+  @pragma('vm:prefer-inline')
   static Iterable<(String, String)> variables() =>
       Platform.environment.entries.map((entry) => (entry.key, entry.value));
 
+  @pragma('vm:prefer-inline')
   static void setCurrentDirectory(String currentDirectory) => Directory.current = currentDirectory;
 
-  
+  //************************************************************************//
+
+  /// Path to a directory where the application may place application-specific cache files.
+  @pragma('vm:prefer-inline')
+  static Future<Directory> getApplicationCacheDirectory() => getApplicationCacheDirectory();
+
+  /// Path to a directory where the application may place data that is user-generated, or that cannot otherwise be recreated by your application.
+  @pragma('vm:prefer-inline')
+  static Future<Directory> getApplicationDocumentsDirectory() => getApplicationDocumentsDirectory();
+
+  /// Path to a directory where the application may place application support files.
+  @pragma('vm:prefer-inline')
+  static Future<Directory> getApplicationSupportDirectory() => getApplicationSupportDirectory();
+
+  /// Path to the directory where downloaded files can be stored.
+  @pragma('vm:prefer-inline')
+  static Future<Directory?> getDownloadsDirectory() => getApplicationSupportDirectory();
+
+  /// Paths to directories where application specific cache data can be stored externally.
+  @pragma('vm:prefer-inline')
+  static Future<List<Directory>?> getExternalCacheDirectories() => getExternalCacheDirectories();
+
+  /// Paths to directories where application specific data can be stored externally.
+  @pragma('vm:prefer-inline')
+  static Future<List<Directory>?> getExternalStorageDirectories({StorageDirectory? type}) => getExternalStorageDirectories(type: type);
+
+  /// Path to a directory where the application may access top level storage.
+  @pragma('vm:prefer-inline')
+  static Future<Directory?> getExternalStorageDirectory() => getExternalStorageDirectory();
+
+  /// Path to the directory where application can store files that are persistent, backed up, and not visible to the user, such as sqlite.db.
+  @pragma('vm:prefer-inline')
+  static Future<Directory> getLibraryDirectory() => getLibraryDirectory();
+
+  /// Path to the temporary directory on the device that is not backed up and is suitable for storing caches of downloaded files.
+  @pragma('vm:prefer-inline')
+  static Future<Directory> getTemporaryDirectory() => getTemporaryDirectory();
+
 }
 
 /// Error type for when an environment variable is not present.
